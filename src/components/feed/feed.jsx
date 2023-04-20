d7f728ba0af16a47bdfed9efe5c034a7e36a6987
khadija <khadijanoorhashmi@gmail.com>
2023-04-20 16:58:41 +0500

    data changes

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,20 +1,17 @@
 import "./feed.css";
 import Share from "../share/share";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
     return (
 <div className="feed">
 <div className="feedWrapper">
 <Share/>
-<Post/>
-<Post/>
-<Post/>
-<Post/>
-<Post/>
-<Post/>
 </div>
-    
+    {Posts.map((p)=>(
+        <Post key={p.id} post={p}/>
+    ))}
 </div>
     );
 }
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,34 +1,40 @@
 import "./Post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Users } from "../../dummyData";
 
-export default function Post() {
+export default function Post({post}) {
+
+    const user= Users.filter((u)=> u.id === post.userId)[0].username;
+    console.log(user)
     return (
 <div className="post">
 <div className="postWrapper">
 
 <div className="postTop">
     <div className="postTopLeft">
-        <img className="postProfileImg" src="/assets/person/1.jpeg" />
-        <span className="postUserName">Khadija Noor Hashmi</span>
-        <span className="postDate">5 min ago</span>
+        <img className="postProfileImg" src={Users.filter((u)=> u.id === post.userId)[0].profilePicture} />
+        <span className="postUserName">
+            {Users.filter((u)=> u.id === post.userId)[0].username}
+        </span>
+        <span className="postDate">{post?.date}</span>
     </div>
     <div className="postTopRight">
         <MoreVertIcon/>
     </div>
 </div>
 <div className="postCenter">
-    <span className="postText">Hey It's my first post</span>
-    <img className="postImg" src="/assets/post/1.jpeg" alt=""/>
+    <span className="postText">{post.desc}</span>
+    <img className="postImg" src={post.photo} alt=""/>
 </div>
 <div className="postBottom">
     <div className="postBottomLeft">
     <img className="likeIcon" src="/assets/heart.png" alt=""/>
     <img className="likeIcon" src="/assets/like.png" alt=""/>
-    <span className="postLikeCounter">32 people like it</span>
+    <span className="postLikeCounter">{post.like} people like it</span>
 
     </div>
     <div className="postBottomRight">
-    <span className="postCommentText">8 commnets</span>
+    <span className="postCommentText">{post.comments} commnets</span>
 
     </div>
 
