47641ac69fbc1ddda931d9cb9fbc029a4e68fc7a
khadija <khadijanoorhashmi@gmail.com>
2023-05-08 23:40:38 +0500

    register page changes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,16 +10,16 @@ export default function Login() {
     <div className="loginWrapper">
     <div className="loginLeft">
         <h1 className="loginLeftTitle">Reactsocial</h1>
-        <span className="loginLeftDesc">Connect with your friends and the world around you on Reactsocial</span>
+        <span className="loginLeftDesc">Connect with your friends and the world around you on Reactsocial.</span>
     </div>
     <div className="loginRight">
 
     <div className="loginForm">
     <input placeholder="Email" className="loginInput" />
     <input placeholder="Password" className="loginInput"/>
-    <button className="loginButton">Sign Up</button>
+    <button className="loginButton">Log In</button>
     <span className="loginForget">Forget Password?</span>
-    <button className="loginRegistrationButton">LoginIn</button>
+    <button className="loginRegistrationButton">Create a New Account</button>
 
 
     </div>
