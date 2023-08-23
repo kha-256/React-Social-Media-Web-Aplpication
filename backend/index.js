363129ad31ceb8693a3b5239a79ba301f44ea5f4
khadija <khadijanoorhashmi@gmail.com>
2023-08-23 23:32:01 +0500

    backend api setup

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const cors = require("cors")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const userRoute= require("./routes/user")
+const authRoute= require("./routes/auth")
 
 dotenv.config()
+
+const corsOptions = {
+  origin: '*',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  exposedHeaders: "Custom-Header1, Custom-Header2",
+}
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
@@ -15,6 +26,15 @@ mongoose.connect(process.env.MONGO_URL)
   .catch((error) => {
     console.error('Failed to connect to MongoDB:', error);
   });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user",userRoute);
+
+app.use("/api/auth",authRoute);
 
 app.listen(8800,()=>{
     console.log('Backend Server is running!')
