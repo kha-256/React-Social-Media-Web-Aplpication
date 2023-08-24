431ba0c0ff1908d6571dc237ba73464e235cd06a
khadija <khadijanoorhashmi@gmail.com>
2023-08-24 23:18:47 +0500

    signup api

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema= new mongoose.Schema({
-    userName: { type: String, required: true, min:3, max:20, unique:true},
+    username: { type: String, required: true, min:3, max:20, unique:true},
     email: { type: String, required: true, unique: true, max:50 },
     password: { type: String, required: true, min:6 },
     profilePicture: { type: String, default:"" },
