c6ab87e897f79fd41d4c793e0fb378d61a7d3caa
khadija <khadijanoorhashmi@gmail.com>
2023-08-27 23:10:51 +0500

    Update api

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,7 +8,11 @@ const userSchema= new mongoose.Schema({
     coverPicture: { type: String, default:"" },
     followers:{type:Array, default:[]},
     followings:{type:Array, default:[]},
-    isAdmin:{type:Boolean, default: false}
+    isAdmin:{type:Boolean, default: false},
+    desc:{type: String, max:50},
+    city:{type: String, max:50},
+    from:{type: String, max:50},
+    relationship:{type: Number,enum:[1,2,3]} //enum are the option numbers
 },
 { timestamps: true }
 );
