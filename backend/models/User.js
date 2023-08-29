97bcb0ed855440a5428bbe474b4ae866d343822c
khadija <khadijanoorhashmi@gmail.com>
2023-08-29 21:47:24 +0500

    post api's

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema= new mongoose.Schema({
-    username: { type: String, required: true, min:3, max:20, unique:true},
-    email: { type: String, required: true, unique: true, max:50 },
-    password: { type: String, required: true, min:6 },
-    profilePicture: { type: String, default:"" },
-    coverPicture: { type: String, default:"" },
-    followers:{type:Array, default:[]},
-    followings:{type:Array, default:[]},
-    isAdmin:{type:Boolean, default: false},
-    desc:{type: String, max:50},
-    city:{type: String, max:50},
-    from:{type: String, max:50},
-    relationship:{type: Number,enum:[1,2,3]} //enum are the option numbers
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, min: 3, max: 20, unique: true },
+    email: { type: String, required: true, unique: true, max: 50 },
+    password: { type: String, required: true, min: 6 },
+    profilePicture: { type: String, default: "" },
+    coverPicture: { type: String, default: "" },
+    followers: { type: Array, default: [] },
+    followings: { type: Array, default: [] },
+    isAdmin: { type: Boolean, default: false },
+    desc: { type: String, max: 50 },
+    city: { type: String, max: 50 },
+    from: { type: String, max: 50 },
+    relationship: { type: Number, enum: [1, 2, 3] } //enum are the option numbers
 },
-{ timestamps: true }
+    { timestamps: true }
 );
 
-module.exports= mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
