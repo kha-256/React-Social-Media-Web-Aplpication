38c3e539ff32d876d296610d52bced2a71d10ebb
khadija <khadijanoorhashmi@gmail.com>
2023-09-01 01:44:38 +0500

    creating routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,11 +3,20 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/Register/Register";
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
 
   return (
-<Home></Home>
+<Routes>
+  <Route  path="/" element={<Login/>} />
+  <Route  path="/register" element={<Register/>} />
+  <Route  path="/home" element={<Home/>} />
+  <Route  path="/profile" element={<Profile/>} />
+
+
+
+</Routes>
 
   );
 }
