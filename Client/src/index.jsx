49a6233354a2da87e54839a215db5e56406304c5
khadija <khadijanoorhashmi@gmail.com>
2023-09-02 23:45:26 +0500

    reduxtoolkit store creation

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux'
+import store from './store/main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-  <BrowserRouter>
+  <Provider store={store}>
+<BrowserRouter>
   <App />
   </BrowserRouter>
+  </Provider>
+  
     
 );
 
