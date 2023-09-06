923100cc22b9cd50b8ce16791988c98236b382bd
khadija <khadijanoorhashmi@gmail.com>
2023-09-06 21:56:35 +0500

    login api Integration

--- Client/src/store/main.jsx
+++ Client/src/store/main.jsx
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import Userslice from "./slices/Userslice";
 
 const store= configureStore({
     reducer:{
-
+        user: Userslice
     }
 })
 
