52df4e4dbce15944bb65b05a5e37155fe1691d9f
khadija <khadijanoorhashmi@gmail.com>
2023-09-07 21:49:58 +0500

    Signup api Integration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,19 +3,22 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/Register/Register";
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route ,} from 'react-router-dom';
 import { Provider } from "react";
+import { Navigate } from "react-router-dom";
 
 
 
 function App() {
+  const user = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
 
   return (
 <Routes>
-  <Route  path="/" element={<Login/>} />
+  <Route  path="/" element={user && token ? <Navigate to="/home" /> : <Login />}  />
   <Route  path="/register" element={<Register/>} />
-  <Route  path="/home" element={<Home/>} />
-  <Route  path="/profile" element={<Profile/>} />
+  {user && token && <Route path="/home" element={<Home />} />}
+  {user && token && <Route path="/profile" element={<Profile />} />}
 
 
 
