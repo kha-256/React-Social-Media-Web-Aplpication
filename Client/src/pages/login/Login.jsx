205999f450abb177b9f1a454631bd3b6356fc779
khadija <khadijanoorhashmi@gmail.com>
2023-09-08 22:35:58 +0500

    login api Integration

--- Client/src/pages/login/Login.jsx
+++ Client/src/pages/login/Login.jsx
@@ -11,18 +11,18 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export default function Login() {
 
-  const [formData, setFormData]= useState({
-    email:"",
-    password:""
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
   });
 
   const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const handleInput=(event)=>{
-    const name=event.target.name;
-    const value= event.target.value
-    setFormData((prev)=>{
-      return {...prev,[name]: value}
+  const handleInput = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
+    setFormData((prev) => {
+      return { ...prev, [name]: value }
     })
   }
 
@@ -30,22 +30,22 @@ export default function Login() {
     setPasswordVisible(!passwordVisible);
   };
 
-  const navigate= useNavigate();
-  const dispatch= useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     // Log the data before sending it to the backend
     console.log("Email:", formData.email);
     console.log("Password:", formData.password);
-  
+
     if (formData.email !== "" && formData.password !== "") {
 
       try {
         const response = await dispatch(userLogin(formData));
         console.log('API response: ', response);
-  
+
         if (response.error) {
           console.log("Error during login:", response.error);
           // Handle the error here (e.g., display it to the user)
@@ -64,58 +64,59 @@ export default function Login() {
         // Handle other errors if needed
         console.log("Error during login:", error.message);
       }
-     
-    } else{
+
+    } else {
       alert("Please fill in the required fields");
     }
   }
-  
 
+  const { loading } = useSelector((state) => state.user)
 
-  const registerNavigation=()=>{
-    
-      navigate("/register")
+  const registerNavigation = () => {
+
+    navigate("/register")
   }
 
-    return (
+  return (
 
-      <>
-<div className="login">
-    <div className="loginWrapper">
-    <div className="loginLeft">
-        <h1 className="loginLeftTitle">Social Media App</h1>
-        <span className="loginLeftDesc">Connect with your friends and the world around you on Social Media App.</span>
-    </div>
-    <div className="loginRight">
+    <>
+      <div className="login">
+        <div className="loginWrapper">
+          <div className="loginLeft">
+            <h1 className="loginLeftTitle">Social Media App</h1>
+            <span className="loginLeftDesc">Connect with your friends and the world around you on Social Media App.</span>
+          </div>
+          <div className="loginRight">
 
-    <form className="loginForm" onSubmit={handleSubmit}>
-    <input placeholder="Email" className="loginInput" 
-      name="email"
-      value={formData.email}
-      onChange={handleInput}
-    />
-    <input placeholder="Password" className="loginInput" 
-      type={passwordVisible ? 'text' : 'password'} // Set the input type conditionally
-    name="password"
-    value={formData.password}
-    onChange={handleInput}
-    />
- <span onClick={togglePasswordVisibility} className="password-toggle">
-        {passwordVisible ? 'Show Password' : 'Show Password'}
-      </span>
-    <button className="loginButton" type="submit">Log In</button>
-    <span className="loginForget">Forget Password?</span>
-    <button className="loginRegistrationButton" onClick={registerNavigation}>Create a New Account</button>
+            <form className="loginForm" onSubmit={handleSubmit}>
+              <input placeholder="Email" className="loginInput"
+                name="email"
+                value={formData.email}
+                onChange={handleInput}
+              />
+              <input placeholder="Password" className="loginInput"
+                type={passwordVisible ? 'text' : 'password'} // Set the input type conditionally
+                name="password"
+                value={formData.password}
+                onChange={handleInput}
+              />
+              <span onClick={togglePasswordVisibility} className="password-toggle">
+                {passwordVisible ? 'Show Password' : 'Show Password'}
+              </span>
+              <button className="loginButton" type="submit">
+                {loading ? 'Loading' : 'Login'}
+              </button>
+              <span className="loginForget">Forget Password?</span>
+              <button className="loginRegistrationButton" onClick={registerNavigation}>Create a New Account</button>
 
 
-    </form>
-    </div>
+            </form>
+          </div>
 
-    </div>
-</div>
-      </>
-    
-    );
-  }
-  
-  
+        </div>
+      </div>
+    </>
+
+  );
+}
+
