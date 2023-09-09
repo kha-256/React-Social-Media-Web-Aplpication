fb9fdfd114f0dd0e7ef4e6f11bc2359183e926ab
khadija <khadijanoorhashmi@gmail.com>
2023-09-09 23:20:31 +0500

    Asset images settings

--- Client/src/components/closeFriend/CloseFriend.jsx
+++ Client/src/components/closeFriend/CloseFriend.jsx
@@ -3,9 +3,11 @@ import "./closeFriend.css";
 
 
 export default function CloseFriend({user}) {
+
+    const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER;
     return (
     <li className="sidebarFriendListItems">
-<img className="sidebarFriendImg" src={user.profilePicture} alt="friend Image"/>
+<img className="sidebarFriendImg" src={`${PF}${user.profilePicture}`} alt="friend Image"/>
 <span className="sidebarFriendName">{user.username}</span>
 
 </li>
