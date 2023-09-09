d4cc6354c4c30b9fbfad22c6d4984d1d3d7279ba
khadija <khadijanoorhashmi@gmail.com>
2023-09-09 23:23:04 +0500

    Asset images settings

--- Client/src/pages/profile/Profile.jsx
+++ Client/src/pages/profile/Profile.jsx
@@ -7,6 +7,8 @@ import Rightbar from "../../components/rightbar/Rightbar";
 
 
 export default function Profile() {
+
+  const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER;
     return (
       <>
 <Topbar/>
@@ -15,8 +17,8 @@ export default function Profile() {
 <div className="profileRight">
 <div className="profileRightTop">
 <div className="profileCover">
-<img className="profileCoverImg" src="/assets/post/3.jpeg" alt=""/>
- <img className="profileUserImg" src="/assets/person/7.jpeg" alt=""/>
+<img className="profileCoverImg" src={`${PF}post/3.jpeg` }alt=""/>
+ <img className="profileUserImg" src={`${PF}person/7.jpeg`} alt=""/>
 </div>
 
 <div className="profileInfo">
