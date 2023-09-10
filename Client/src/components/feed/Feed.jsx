2bb263a8fa8df6266e745799b54a66e419932013
khadija <khadijanoorhashmi@gmail.com>
2023-09-10 23:14:33 +0500

    Get posts api integration

--- Client/src/components/feed/Feed.jsx
+++ Client/src/components/feed/Feed.jsx
@@ -2,16 +2,37 @@ import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
 import { Posts } from "../../dummyData";
+import { getTimelinePost } from "../../store/slices/PostSlice";
+import { useDispatch,useSelector } from "react-redux";
+import { useEffect,  } from "react";
 
 export default function Feed() {
+
+    const dispatch=useDispatch();
+    const user = JSON.parse(localStorage.getItem('user'));
+    const userId = user._id;
+    
+    useEffect(()=>{
+        dispatch(getTimelinePost(userId))
+
+    },[dispatch,userId])
+
+
+    
+const posts = useSelector(state => state.post.posts);
+
+console.log(posts)
+
+
+
     return (
 <div className="feed">
 <div className="feedWrapper">
 <Share/>
 </div>
-    {Posts.map((p)=>(
-        <Post key={p.id} post={p}/>
-    ))}
+   {posts && posts.map((post) => (
+                    <Post key={post._id} post={post} />
+                ))}
 </div>
     );
 }
