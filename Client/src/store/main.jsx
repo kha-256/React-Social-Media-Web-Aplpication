4f54ebc5d55edebe52246aac514328782a70bb42
khadija <khadijanoorhashmi@gmail.com>
2023-09-10 23:14:19 +0500

    Get posts api integration

--- Client/src/store/main.jsx
+++ Client/src/store/main.jsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import Userslice from "./slices/Userslice";
+import PostSlice from "./slices/PostSlice";
+import PostUser from "./slices/PostUser";
 
 const store= configureStore({
     reducer:{
-        user: Userslice
+        user: Userslice,
+        post: PostSlice,
+        postuser:PostUser,
     }
 })
 
