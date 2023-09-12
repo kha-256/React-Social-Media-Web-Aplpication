e0874bfcea33c830c0b9221893390d7846d7aa44
khadija <khadijanoorhashmi@gmail.com>
2023-09-12 21:43:22 +0500

    Get posts api integration

--- Client/src/components/post/Post.jsx
+++ Client/src/components/post/Post.jsx
@@ -1,33 +1,49 @@
 import "./Post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Users } from "../../dummyData";
+import { getUser } from "../../store/slices/PostUser";
+import { useDispatch,useSelector } from "react-redux";
+import { useEffect,  } from "react";
 import { useState } from "react";
+import {format} from "timeago.js"
 
 export default function Post({post}) {
 
     const [like, setLike]=useState(post.like);
     const [isLiked, setIsLiked]=useState(false)
 
+    const dispatch=useDispatch();
+    
+ 
+    useEffect(() => {
+        dispatch(getUser(post.userId));
+    }, [dispatch, post.userId]);
+    
+    const postUser = useSelector(state => state.postuser.postUser);
+    
+    console.log(postUser);
+
     const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER;
-    console.log('these are', PF);
 
     const likeHandler=()=>{
         setLike(isLiked ? like-1: like+1)
         setIsLiked(!isLiked)
     }
-    const user= Users.filter((u)=> u.id === post.userId)[0].username;
-    console.log(user)
+
+    const user = useSelector(state => state.postuser.postUser);
+
+
+   // const user= Users.filter((u)=> u.id === post.userId)[0].username;
+   // console.log(user)
     return (
 <div className="post">
+{user &&
 <div className="postWrapper">
 
 <div className="postTop">
     <div className="postTopLeft">
-        <img className="postProfileImg" src={Users.filter((u)=> u.id === post.userId)[0].profilePicture} />
-        <span className="postUserName">
-            {Users.filter((u)=> u.id === post.userId)[0].username}
-        </span>
-        <span className="postDate">{post?.date}</span>
+       <img className="postProfileImg" src={user.profilePicture || PF+"person/Default_Profile_Picture.jpg"} /> 
+        <span className="postUserName"> {user.username} </span>
+        <span className="postDate">{format(post.createdAt)}</span>
     </div>
     <div className="postTopRight">
         <MoreVertIcon/>
@@ -35,13 +51,13 @@ export default function Post({post}) {
 </div>
 <div className="postCenter">
     <span className="postText">{post.desc}</span>
-    <img className="postImg" src={`${PF}${post.photo}`}  alt=""/>
+    <img className="postImg" src={`${PF}${post.img}`}  alt=""/>
 </div>
 <div className="postBottom">
     <div className="postBottomLeft">
     <img className="likeIcon" src={`${PF}like.png`} alt="" onClick={likeHandler}/>
     <img className="likeIcon" src={`${PF}heart.png`} alt="" onClick={likeHandler}/>
-    <span className="postLikeCounter">{like} people like it</span>
+    <span className="postLikeCounter">{post.likes.length} people like it</span>
 
     </div>
     <div className="postBottomRight">
@@ -53,6 +69,8 @@ export default function Post({post}) {
 
 
 </div>
+
+}
 
 </div>
     );
